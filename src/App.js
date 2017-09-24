33bfc3829b44619479b10d11716c677cbadbb829
Danielle Gellis <danisyellis@gmail.com>
2017-09-23 21:24:50 -0700

    move 8-ball to its own component and add click functionality

--- src/App.js
+++ src/App.js
@@ -10,41 +10,26 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Tap the magic eight ball below to reveal your fortune.</h2>
-          <input type="image" src={predictionBall} className="prediction-ball" alt="eight ball showing the prediction screen" />
+          <FortuneBall />
         </div>
       </div>
     );
   }
 }
 
-class TheFortunes extends Component {
-  //state for what is inside of the triangle
-  .setState(fortuneOfTheMoment)
-  getFortune(fortune) {  //a function that randomizes getting the fortune and then returns the xml component
-    return <FortuneTriangle/>
-
+class FortuneBall extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value:null,
+    }
   }
   render() {
-    return ({this.getFortune(currentFortune)}
-    );
+    return (
+      <input type="image" src={predictionBall} className="prediction-ball" alt="eight ball showing the prediction screen" onClick={() => {alert("I was clicked!")}} />
+    )
   }
 }
-
-const FortuneTriangle = function (props) {
-  return {
-    props, // set props
-    render () {
-      // get `word` from props obj with
-      const { fortune } = this.props;
-      return (
-        <p> { fortune }!</p>
-      );
-    }
-  };
-};
-
-
-//can return FortuneTriangle inside of the
 
 
 export default App;
