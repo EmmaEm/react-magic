4c621a6df588c11b3bf520a6137d09427886a020
Danielle Gellis <danisyellis@gmail.com>
2017-09-23 17:56:05 -0700

    make the ball a button that does things(WIP)

--- src/App.js
+++ src/App.js
@@ -10,14 +10,41 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Tap the magic eight ball below to reveal your fortune.</h2>
-          <img src={predictionBall} className="prediction-ball" alt="eight ball showing the prediction screen" />
+          <input type="image" src={predictionBall} className="prediction-ball" alt="eight ball showing the prediction screen" />
         </div>
-        {/* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
       </div>
     );
   }
 }
+
+class TheFortunes extends Component {
+  //state for what is inside of the triangle
+  .setState(fortuneOfTheMoment)
+  getFortune(fortune) {  //a function that randomizes getting the fortune and then returns the xml component
+    return <FortuneTriangle/>
+
+  }
+  render() {
+    return ({this.getFortune(currentFortune)}
+    );
+  }
+}
+
+const FortuneTriangle = function (props) {
+  return {
+    props, // set props
+    render () {
+      // get `word` from props obj with
+      const { fortune } = this.props;
+      return (
+        <p> { fortune }!</p>
+      );
+    }
+  };
+};
+
+
+//can return FortuneTriangle inside of the
+
 
 export default App;
