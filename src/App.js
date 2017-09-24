f4defeee1d88e3121a0fb99b51feb3b7035f2467
Danielle Gellis <danisyellis@gmail.com>
2017-09-23 22:56:25 -0700

    fortune is in the ball and awesome fortunes added

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Tap the magic eight ball below to reveal your fortune.</h2>
-          <FortuneBall />
         </div>
+        <FortuneBall />
       </div>
     );
   }
@@ -26,14 +26,14 @@ class FortuneBall extends Component {
   }
 
   shakeBall() {
-    const fortunes = ["Yes", "No", "Maybe"];
+    const fortunes = ["Yes", "No", "Maybe", "You trippin'","You will have to be patient", "It is decidedly so", "Better not tell you now", "My sources say no", "My sources say yes", "Hell no", "Make it so", "Concentrate and ask again", "Outlook not so good", "Very doubtful", "Without a doubt", "Most likely", "If you insist", "Why would you even ask that", "Pervert."];
     const fortune = fortunes[Math.floor(Math.random()*fortunes.length)]
     this.setState({fortune: fortune});
   }
 
   render() {
     return (
-      <div>
+      <div className="ballAndFortune">
         <Triangle value={this.state}/>
         <input type="image" src={predictionBall} className="prediction-ball" alt="eight ball showing the prediction screen" onClick={() => this.shakeBall()} />
       </div>
@@ -44,7 +44,7 @@ class FortuneBall extends Component {
 function Triangle(props) {
   console.log(props);
   return (
-    <p>{props.value.fortune}</p>
+    <p className = "fortune">{props.value.fortune}</p>
   )
 }
 
